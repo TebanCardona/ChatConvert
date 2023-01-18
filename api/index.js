ba04cdc58929b596a5aa8441db93866613539aa3
Esteban Cardona <estebanfabi737@gmail.com>
2023-01-18 01:24:17 -0500

    try deploy backend

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { postCurrencies } = require('./src/scripts/postCurrencies')
 // Syncing all the models at once.
+const port = process.env.PORT || 3001;
+
 conn.sync({ force: false }).then(() => {
-  // postCurrencies()
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  postCurrencies()
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
