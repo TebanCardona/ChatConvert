4fd660cb9e92b67758215a7e2f842556d11dca1e
Esteban Cardona <estebanfabi737@gmail.com>
2023-01-18 03:00:59 -0500

    for one letter

--- api/src/models/currencies.js
+++ api/src/models/currencies.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define(
-    "currency",
+    "currencies",
     {
       id: {
         type: DataTypes.STRING,
