188e3afd03395dd3e12bb1d59495c6d268d6eb5e
Esteban Cardona <estebanfabi737@gmail.com>
2023-01-18 02:36:46 -0500

    se me olvido cors xD

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); //req.body (el objeto contendrá valores de cualquier tipo y no solo cadenas)
+app.use(bodyParser.urlencoded({ extended: true }));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
